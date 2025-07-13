5431a2d4002e459e032b8a5f7a7ecd28f0cf5db8
BillMcPhil <125710492+BillMcPhil@users.noreply.github.com>
2025-07-13 17:29:55 -0600

    Added skeleton login page

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <>
+            <span>LOGIN</span>
+
+            <form>
+                <input type="text" placeholder="username" />
+                <input type="text" placeholder="password" />
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
+}
